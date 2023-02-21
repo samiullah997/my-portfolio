0ff39ec2a9ba0dcfcd02ffa0035b5ab58acaef8d
samiullah997 <samiullahk997@gmail.com>
2023-02-21 21:42:26 +0500

    fix linter errors

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,23 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-title.png';
 import './index.scss';
-const Home =()=> {
-    return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>Hi, <br />I'm
-                <img src={LogoTitle} alt="developer" />
-                Lobonan
-                <br/>
-                web developer
-                </h1>
-                <h2>Full Stack Developer / Front End Developer / Back End Developer</h2>
-                <Link to="/contact" className="flat-button">CONTACT ME</Link>
-            </div>
-        </div>
-    );
-};
+
+const Home = () => (
+  <div className="container home-page">
+    <div className="text-zone">
+      <h1>
+        Hi&apos;
+        <br />
+        I&apos;m
+        <img src={LogoTitle} alt="developer" />
+        Lobonan
+        <br />
+        web developer
+      </h1>
+      <h2>Full Stack Developer / Front End Developer / Back End Developer</h2>
+      <Link to="/contact" className="flat-button">CONTACT ME</Link>
+    </div>
+  </div>
+);
 
 export default Home;
